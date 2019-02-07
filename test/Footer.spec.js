a958948bb0a1c89580e4200561f51d2b1391f929
Miguel Ángel <migueabellan@gmail.com>
2019-02-07 16:06:36 +0100

    Add tests

---
+++ test/Footer.spec.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
+import Footer from '@/components/Layout/Footer.vue'
+import Vuetify from 'vuetify'
+
+describe('Footer', () => {
+  
+  beforeEach(() => {
+    Vue.use(Vuetify)
+  })
+
+  test('is a Vue instance', () => {
+    const wrapper = mount(Footer)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+})
