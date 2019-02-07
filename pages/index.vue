6ddf1b8eae62242d56057228f265c6461165def0
Miguel Ángel <migueabellan@gmail.com>
2019-02-07 12:53:19 +0100

    Update logo

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <v-layout justify-center align-center>
       <v-flex shrink>
         <div class="main">
-          <img src="/elcableamarillo.png" title="Proyecto Educativo El Cable Amarillo" />
+          <img src="~/static/elcableamarillo.png" title="Proyecto Educativo El Cable Amarillo" />
           <h1 class="display-1 font-weight-thin">Proyecto educativo de código abierto para fomentar el uso de la <strong class="font-weight-black">programación y robótica</strong> en los centros educativos utilizando herramientas de <strong class="font-weight-black">software y hardware libre</strong>.</h1>
         </div>
       </v-flex>
