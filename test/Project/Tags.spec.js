6359310e6e4021e7bc6349db18ed4c4a2f823b6a
Miguel Ángel <migueabellan@gmail.com>
2019-02-09 10:10:39 +0100

    Add test tags

---
+++ test/Project/Tags.spec.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
+import Tags from '@/components/Project/Tags.vue'
+import Vuetify from 'vuetify'
+
+const tags = { 
+  tags: 'tag-1, tag-2, tag-3'
+}
+
+describe('Tags', () => {
+  beforeEach(() => {
+    Vue.use(Vuetify)
+  })
+  test('is a Vue instance', () => {
+    const wrapper = mount(Tags, { propsTags: { tags } })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
