c98f727626e0ce0c15099842a891759d091b07f2
Miguel Ángel <migueabellan@gmail.com>
2019-02-09 10:10:31 +0100

    Add test authors

---
+++ test/Project/Authors.spec.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
+import Authors from '@/components/Project/Authors.vue'
+import Vuetify from 'vuetify'
+
+const authors = { 
+  authors: [ 
+    { name: 'Name 1', github: 'username-1' },
+    { name: 'Name 2', github: '' } 
+  ],
+}
+
+describe('Authors', () => {
+  beforeEach(() => {
+    Vue.use(Vuetify)
+  })
+  test('is a Vue instance', () => {
+    const wrapper = mount(Authors, { propsAuthors: { authors } })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
