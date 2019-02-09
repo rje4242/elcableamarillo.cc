38938e0abea31998dca2632c92877c657d28fe07
Miguel Ángel <migueabellan@gmail.com>
2019-02-09 10:10:22 +0100

    Add test info

---
+++ test/Project/Info.spec.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import { mount } from '@vue/test-utils'
+import Info from '@/components/Project/Info.vue'
+import Vuetify from 'vuetify'
+
+const data = { 
+  level: 'Level',
+  category: 'Category',
+}
+
+describe('Info', () => {
+  beforeEach(() => {
+    Vue.use(Vuetify)
+  })
+  test('is a Vue instance', () => {
+    const wrapper = mount(Info, { propsData: { data } })
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+})
