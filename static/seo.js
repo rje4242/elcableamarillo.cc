b05da82ddc3dae000573acf05e7414fba2447cea
Miguel Ángel <migueabellan@gmail.com>
2019-02-11 15:36:55 +0100

    Add image tag

--- static/seo.js
+++ static/seo.js
@@ -11,26 +11,31 @@ export default {
     title: 'Proyecto Educativo - El Cable Amarillo',
     description:
       'Proyecto educativo de código abierto para fomentar el uso de la programación y robótica en los centros educativos utilizando herramientas de software y hardware libre',
-    keywords: 'proyecto, open source, software libre, hardware libre'
+    keywords: 'proyecto, open source, software libre, hardware libre',
+    image: ''
   },
   practicas: {
     title: 'Prácticas',
     description: 'Prácticas de programación y robótica',
-    keywords: 'prácticas, programación, robótica'
+    keywords: 'prácticas, programación, robótica',
+    image: ''
   },
   primaria: {
     title: 'Prácticas Primaria',
     description: 'Prácticas de programación y robótica para educación primaria',
-    keywords: 'prácticas, programación, robótica, mblock, primaria'
+    keywords: 'prácticas, programación, robótica, mblock, primaria',
+    image: ''
   },
   secundaria: {
     title: 'Prácticas Secundaria',
     description: 'Prácticas de programación y robótica para educación primaria',
-    keywords: 'prácticas, programación, robótica, mblock, secundaria'
+    keywords: 'prácticas, programación, robótica, mblock, secundaria',
+    image: ''
   },
   bachillerato: {
     title: 'Prácticas Bachillerato',
     description: 'Prácticas de programación y robótica para bachillerato',
-    keywords: 'prácticas, programación, robótica, arduino, bachillerato'
+    keywords: 'prácticas, programación, robótica, arduino, bachillerato',
+    image: ''
   }
 }
