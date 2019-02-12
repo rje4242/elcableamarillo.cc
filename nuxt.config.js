bfa0b0285eaf2d1da7a0ffcf627e54874105fab2
Miguel Ángel <migueabellan@gmail.com>
2019-02-12 18:11:15 +0100

    Update config

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ module.exports = {
       { name: 'robots', content: 'index, follow' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: `${canonical}/favicon.ico` },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
         href:
