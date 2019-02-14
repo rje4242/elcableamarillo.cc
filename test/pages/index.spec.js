bc0fe56fb18ed8ae0a490ba73f5f6bd36cfa0b02
Miguel Ángel <migueabellan@gmail.com>
2019-02-14 13:11:44 +0100

    Add test

---
+++ test/pages/index.spec.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import { shallowMount } from '@vue/test-utils'
+import Index from '@/pages/index.vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify)
+
+
+describe('Index', () => {
+  test('mounts properly', () => {
+    const wrapper = shallowMount(Index)
+    expect(wrapper.isVueInstance()).toBeTruthy()
+  })
+
+  test('renders properly', () => {
+    const wrapper = shallowMount(Index)
+    expect(wrapper.html()).toMatchSnapshot()
+    expect(wrapper.html()).toContain('Proyecto')
+  })
+})
