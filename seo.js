45e882aedc76f65b1976819e4642d3c2dc567e57
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-01 18:34:46 +0000

    Deploy ElCableAmarillo/elcableamarillo.cc to github.com/ElCableAmarillo/elcableamarillo.cc.git:gh-pages

---
+++ seo.js
@@ -0,0 +1,59 @@
+export default {
+  default: {
+    title: 'El Cable Amarillo',
+    description:
+      'Proyecto educativo de código abierto para fomentar el uso de la programación y robótica en los centros educativos utilizando herramientas de software y hardware libre',
+    keywords:
+      'programación, robótica, arduino, educación, primaria, secundaria, murcia',
+    image: 'http://www.elcableamarillo.cc/default.png'
+  },
+  proyecto: {
+    title: 'Proyecto Educativo - El Cable Amarillo',
+    description:
+      'Proyecto educativo de código abierto para fomentar el uso de la programación y robótica en los centros educativos utilizando herramientas de software y hardware libre',
+    keywords: 'proyecto, open source, software libre, hardware libre',
+    image: ''
+  },
+  formacion: {
+    title: 'Formación',
+    description:
+      'Recursos para la formación de programación y robótica sobre los materiales del programa educativo El Cable Amarillo',
+    keywords: 'formación, programación, robótica',
+    image: ''
+  },
+  practicas: {
+    title: 'Prácticas',
+    description:
+      'Practicas educativas realizadas por docentes para introducir la programación y la robótica en todas las etapas educativas.',
+    keywords: 'prácticas, programación, robótica',
+    image: ''
+  },
+  primaria: {
+    title: 'Prácticas para primaria',
+    description:
+      'Practicas educativas realizadas por docentes para introducir la programación y la robótica en educación primaria.',
+    keywords: 'prácticas, programación, robótica, mblock, primaria',
+    image: ''
+  },
+  secundaria: {
+    title: 'Prácticas para secundaria',
+    description:
+      'Practicas educativas realizadas por docentes para introducir la programación y la robótica en educación secundaria.',
+    keywords: 'prácticas, programación, robótica, mblock, secundaria',
+    image: ''
+  },
+  bachillerato: {
+    title: 'Prácticas para bachillerato',
+    description:
+      'Practicas educativas realizadas por docentes para introducir la programación y la robótica en bachillerato.',
+    keywords: 'prácticas, programación, robótica, arduino, bachillerato',
+    image: ''
+  },
+  docentes: {
+    title: 'Docentes',
+    description:
+      'Listado de docentes que han contribuido en la realización de prácticas de programación y robótica.',
+    keywords: 'docentes, maestros, maestras, profesores, profesoras',
+    image: ''
+  }
+}
